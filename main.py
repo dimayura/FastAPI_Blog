37d989b58e2af280bbd06bda7535015821ea9105
D I Mayura <dimayuranaik@gmail.com>
2026-04-28 23:51:17 +0530

    templates completed

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI, Request
 #from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 #from fastapi.responses import PlainTextResponse
+from fastapi.staticfiles import StaticFiles
 
 app= FastAPI()
 
 templates=Jinja2Templates(directory="templates")
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 posts:list[dict]=[
     {
@@ -28,17 +30,18 @@ posts:list[dict]=[
 # def get_first_post():
 #     return f"<h1>{posts[0]['author']}</h1>"
 
-@app.get("/",include_in_schema=False)
+@app.get("/",include_in_schema=False,name="home")
+@app.get("/posts",include_in_schema=False,name="posts")
 def home(request:Request):
     return templates.TemplateResponse(request,"home.html",{
         "posts":posts,
         "count":len(posts)
         })
 
+@app.get("/get/posts")
+def get_all_posts():
+    return posts
+
 # @app.get("/api/posts", response_class=PlainTextResponse)
 # def get_posts():
 #     return str(posts)
-
-@app.get("/api/posts")
-def get_posts():
-    return posts
